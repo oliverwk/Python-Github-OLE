8983ffff80510e0c9d3eb1505180cee966be60f9
oliverwk <30601311+oliverwk@users.noreply.github.com>
2020-04-02 20:54:08 +0200

    Update tat.py

--- tat.py
+++ tat.py
@@ -1,5 +1,8 @@
 import os
 import urllib
+import requests
+from bs4 import BeautifulSoup
+import sys
 os.system('clear')
 DOWNLOADS_DIR = '/Users/MWK/Desktop/tatiana'
 
@@ -8,14 +11,31 @@ for url in open('tatiana_down.txt'):
     # Split on the rightmost / and take everything on the right side of that
     name = url.rsplit('/', 1)[-1]
 
+
+    headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15'}
+
+    print("Downloading")
+    print(url)
+
+    page = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    image = soup.find("meta",  property="og:image")
+
+    title = soup.find("meta",  property="og:title")
+
+    print(image["content"] if title else "Er is geen afbeelding.")
+    print("")
+    print("Dit is de beschrijving:")
+    print(title["content"] if title else "Er is geen beschrijving.")
+    print("")
+
     # Combine the name and the downloads directory to get the local filename
-    filename = os.path.join(DOWNLOADS_DIR, name)
+    #filename = os.path.join(DOWNLOADS_DIR, name)
 
     import wget
     #test link googe docs https://docs.google.com/document/d/17WRu2xbcR_yqTmWaebbTjlKUcdCqNb-3wDx5A_SHui8/edit?usp=sharing
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    target = "tatiana_art"
-    prefix_list = ["91018230_151840196296351_1894637158564459764_n"]
     base_dir = os.path.dirname(os.path.abspath(__file__))
     target = "tatiana_art"
     prefix_list = ["91018230_151840196296351_1894637158564459764_n"]
@@ -28,7 +48,7 @@ for url in open('tatiana_down.txt'):
             exit()
 
     #test googe   https://docs.google.com/document/d/17WRu2xbcR_yqTmWaebbTjlKUcdCqNb-3wDx5A_SHui8/edit?usp=sharing
-    local_image_filename = wget.download(url, out=path)
+    local_image_filename = wget.download(image["content"], out=path)
 
 
     x = len(os.listdir('/Users/MWK/Desktop/tatiana/'))+1
@@ -39,5 +59,6 @@ for url in open('tatiana_down.txt'):
     dest = '/Users/MWK/Desktop/tatiana/tatiana_art_{n}.jpg'.format(n=x)
 
     os.rename(source, dest)
+    print("")
     # Download the file if it does not exist
     #urllib.urlretrieve(url, filename)
